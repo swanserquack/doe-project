e0199c91c210db3d56a87ba10ba3709dd87903f4
swanserquack <75944176+swanserquack@users.noreply.github.com>
2022-05-12 20:35:56 +0100

    Updated the main file in order to sniff

--- Firewall.cpp
+++ Firewall.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 #include <tins/tins.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 using namespace Tins;
 using namespace std;
 int option;
 
+bool loop(const PDU &pdu) {
+    const IP &ip = pdu.rfind_pdu<IP>();
+    const TCP &tcp = pdu.rfind_pdu<TCP>();
+    return ip.src_addr();
+    return ip.dst_addr();
+    return true;
+}
+
 int main(){
+    while(true){
+        //Going to put in a temporary memory killswitch here in case of variable overload
+    }
     cout << "Select what to do" << endl;
     cout << "1. Enable/Disable" << endl;
     cout << "2. Settings" << endl;
     cout << "3. Exit" << endl << endl;
     cin >>option;
     if (option == 1){
-
+        //The wlan0 is temporary and will be changed in the future
+        Sniffer("wlan0").sniff_loop(loop);
     }
     else if (option == 2){
 
