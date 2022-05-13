c165df193d3b6bac8fad971ab00f30f2f2c056b7
swanserquack <75944176+swanserquack@users.noreply.github.com>
2022-05-13 20:21:09 +0100

    Cleaned up a bit and added some debugging

--- Firewall.cpp
+++ Firewall.cpp
@@ -1,7 +1,11 @@
+//Going to need to change these to files bundled with the exec maybe?
 #include <iostream>
 #include <tins/tins.h>
-#include "sys/types.h"
-#include "sys/sysinfo.h"
+#include <sys/types.h>
+#include <sys/sysinfo.h>
+#include <sys/resource.h>
+
+struct rlimit limits;
 
 using namespace Tins;
 using namespace std;
@@ -11,20 +15,26 @@ struct sysinfo memInfo;
 bool loop(const PDU &pdu) {
     const IP &ip = pdu.rfind_pdu<IP>();
     const TCP &tcp = pdu.rfind_pdu<TCP>();
+    cout << ip.src_addr() << endl;
     return ip.src_addr();
     return ip.dst_addr();
     return true;
 }
 
 int main(){
-    while(true){
-        //Going to put in a temporary memory killswitch here in case of variable overload
+    limits.rlim_max = 5368709120;
+    int err = setrlimit(RLIMIT_DATA, &limits);
+    if (err){
+        cout << "Memory limit breached, running killswitch" << endl;
+        void _exit(int status);
     }
+
     cout << "Select what to do" << endl;
     cout << "1. Enable/Disable" << endl;
     cout << "2. Settings" << endl;
     cout << "3. Exit" << endl << endl;
     cin >>option;
+
     if (option == 1){
         //The wlan0 is temporary and will be changed in the future
         Sniffer("wlan0").sniff_loop(loop);
