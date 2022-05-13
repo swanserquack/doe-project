6b8c987cbc48055237166c269d32bef8c094fbd6
swanserquack <75944176+swanserquack@users.noreply.github.com>
2022-05-13 20:35:31 +0100

    Added notes and removed unsed memory counter

--- Firewall.cpp
+++ Firewall.cpp
@@ -12,15 +12,18 @@ int option;
 struct sysinfo memInfo;
 
 bool loop(const PDU &pdu) {
-    limits.rlim_max = 5368709120;
+    limits.rlim_max = 5368709120; //Sets hard mem limit to 5Gb
+    // Magic shit
     const IP &ip = pdu.rfind_pdu<IP>();
     const TCP &tcp = pdu.rfind_pdu<TCP>();
     cout << ip.src_addr() << endl;
+    //Mem magic shit
     int err = setrlimit(RLIMIT_DATA, &limits);
     if (err == 1){
         cout << "Memory limit breached, running killswitch" << endl;
         void _exit(int status);
     }
+    //These returns arnt working
     return ip.src_addr();
     return ip.dst_addr();
     return true;
@@ -38,10 +41,7 @@ int main(){
             Sniffer("wlan0").sniff_loop(loop);
         }
         else if (option == 2){
-            sysinfo (&memInfo);
-            long long physMemUsed = memInfo.totalram - memInfo.freeram;
-            physMemUsed *= memInfo.mem_unit;
-            cout << "Physical Memory Used: " << physMemUsed << endl;
+            //Need to add somekind of settings storage (maybe a file?)
         }
         else if (option == 3){
             return 0;
